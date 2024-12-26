3fbe6eb71b1f843b15e521736d82147a12ec2c7b
saadetsimsek <saadetsimsek13@gmail.com>
2024-12-26 16:27:38 +0300

    Initial Commit

---
+++ MotionFlowTests/MotionFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotionFlowTests.swift
+//  MotionFlowTests
+//
+//  Created by Saadet Şimşek on 26/12/2024.
+//
+
+import Testing
+@testable import MotionFlow
+
+struct MotionFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
